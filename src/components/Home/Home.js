0cc9f0380d61343238009f0703c1d192e8c95abb
Mainuddinhasansakib <sakibuddin831@gmail.com>
2020-10-21 11:08:51 +0600

    error faced

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import volunteerServices from '../../FakeData/FakeData';
+import React, { useEffect, useState } from 'react';
+// import volunteerServices from '../../FakeData/FakeData';
 import FreewillService from '../FreewillService/FreewillService';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 
 const Home = () => {
+    const [volunteerItems,setVolunteerItems] = useState([])
+    // const handleAddProduct = () => {
+    //     // fetch('http://localhost:5000/volunteerAllItemsAdd')
+    //     fetch('http://localhost:5000/addProduct', {
+    //         method: 'POST',
+    //         headers: {'Content-Type': 'application/json'},
+    //         body: JSON.stringify(volunteerServices)
+    //     })
+
+    //   }
+
+      useEffect(()=>{
+        fetch('http://localhost:5000/products')
+        .then(res => res.json())
+        .then(data => setVolunteerItems(data))
+      }, [])
+
 
     return (
         <div>
@@ -13,11 +30,13 @@ const Home = () => {
             </div>
             <div className="row card-container">
                 {
-                    volunteerServices.map(vService =><FreewillService key={vService.itemId} vService={vService}></FreewillService>)
+                    // volunteerServices.map(vService =><FreewillService key={vService.itemId} vService={vService}></FreewillService>)
+                    volunteerItems.map(vService =><FreewillService key={vService.itemId} vService={vService}></FreewillService>)
              
                 }
 
             </div>
+            
         </div>
     );
 };
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,22 +1,28 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useParams} from 'react-router-dom';
 import { UserContext } from '../../App';
 import logo from '../../resources/logos/Group 1329.png';
 import { useForm } from "react-hook-form";
-import volunteerServices from '../../FakeData/FakeData';
-
+// import volunteerServices from '../../FakeData/FakeData';
 import './Registration.css'
 
 
 const Registration = () => {
  
   const [loggedInUser, setLoggedInUser] = useContext(UserContext)
-  
-  // const [product, setProduct] = useState({});
-  const { itemId} = useParams();
-  const volunteerItem = volunteerServices.find(vs => vs.uniqueId === itemId )
+  const [freeWillItem,setFreeWillItem] = useState({});
+  const {id} = useParams();
+  // console.log(id)
+
+  const volunteerItem = freeWillItem.find(vs => vs.uniqueId === id )
   console.log(volunteerItem)
   const { register, errors, handleSubmit } = useForm();
+
+  useEffect(() => {
+    fetch('http://localhost:5000/product/'+ id)
+        .then(res => res.json())
+        .then(data => setFreeWillItem(data))
+}, [id])
   // const onSubmit = data => console.log(data);
 
   const onSubmit = data => {
@@ -28,8 +34,8 @@ const Registration = () => {
 
       })
       .then(res => res.json())
-      .then(data => {
-      if (data) {
+      .then(success => {
+      if (success) {
       alert('your order placed successfully')
 
     }
@@ -38,25 +44,8 @@ const Registration = () => {
 
 
 }
-  
-
-    //     const RegisterData = {...loggedInUser}
-    //     fetch('http://localhost:5000/addProduct', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify(RegisterData)
-    //     })
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             if (data) {
-    //                 alert('your order placed successfully')
-    //             }
-    //         })
-    // };
-
-    return (
+ 
+return (
         <div className="registration-container">
           <div>
             <Link to="/home"><img style={{ width: '20%' }} src={logo} alt="logo" /></Link>
@@ -98,68 +87,11 @@ const Registration = () => {
                 {/* <Link to="/bookedItems" > <input className="form-control submitBtn" type="submit" /></Link> */}
                 <input className="form-control submitBtn" type="submit" />
               </div>
-              
-
-          
-
-           
+  
             </div>
         </form>
         
         </div>
-
-        /* <div className="from-group mt-5">
-            <button onClick={handleGoogleSignIn} className="btn btn-brand">Google Sign in</button>
-             </div> */
-               /* <div className="form-group">
-                <label htmlFor="" className="text-danger">Forgot your password?</label>
-              </div> */
-
-
-
-
-
-
-        // <div className="registration-container">
-            
-        //   <div className="row align-items-center" style={{ height: "100vh" }}>
-        //     <div className="col-md-6 shadow p-5">
-        //     <div>
-        //     <Link style={{}} className="mb-5" to="/home"><img style={{ width: '20%', marginLeft:"40%"}} src={logo} alt="logo" /></Link>
-        //     </div>
-        //      <div className="form-group">
-        //         <label htmlFor="">Full Name</label>
-        //         <input type="password" className="form-control" />
-        //       </div>
-        //       <div className="form-group">
-        //         <label htmlFor="">User Name or email</label>
-        //         <input type="text" className="form-control" />
-        //       </div>
-             
-        //       <div className="form-group">
-        //         <label htmlFor="">Date</label>
-        //         <input type="password" className="form-control" />
-        //       </div>
-        //       <div className="form-group">
-        //         <label htmlFor="">Description</label>
-        //         <input type="password" className="form-control" />
-        //       </div>
-        //       <div className="form-group">
-        //         <label htmlFor="">Organize books at the library</label>
-        //         <input type="password" className="form-control" />
-        //       </div>
-        //       <div className="form-group">
-        //         <label htmlFor="" className="text-danger">Forgot your password?</label>
-        //       </div>
-        //       <div className="from-group mt-5">
-        //         <button className="btn btn-brand">Google Sign in</button>
-        //       </div>
-        //     </div>
-        //     {/* <div className="col-md-6 d-none d-md-block align-self-end">
-        //       <img className="img-fluid" src={LoginBg} alt="" />
-        //     </div> */}
-        //   </div>
-        // </div>
       );
     };
 
